10b1bdd3c462e16f738b7d0ec57f09681db662ef
heto-heto <146400600+heto-heto@users.noreply.github.com>
2023-10-09 20:36:56 -0500

    Add files via upload

---
+++ C#/Program_Nombre.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Programa_Nombre;
+
+class Nombre
+{
+    static void Main(string[] args)
+    {
+        string name = "Héctor Alejandro Rueda Briones";
+        Console.WriteLine(name);
+    }
+}
---
+++ C#/Program_Suma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Programa_Suma;
+
+class Suma
+{
+    static void Main(string[] args)
+    {
+        int a = 0, b = 0,suma=0;
+        Console.WriteLine("* * *  Suma de dos números  * * *");
+        Console.Write("Valor de a: ");
+        a=Int32.Parse(Console.ReadLine());
+        Console.Write("Valor de b: ");
+        b=Int32.Parse(Console.ReadLine());
+        suma = a + b;
+        Console.WriteLine("La suam es: "+suma);
+    
+    }
+}
